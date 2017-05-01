6f27db81f858cfe98aaa46531216b8b91665bb27
Giovanni Bernini <giiooh.bernini@hotmail.com>
2017-05-01 01:43:39 -0300

    A47

--- src/components/app-content.js
+++ src/components/app-content.js
@@ -6,8 +6,11 @@ import UserInfo from './user-info'
 import Actions from './actions'
 import Repos from './repos'
 
-/*
+/* Explicações
+
 - o '!!' usado força a variável a retornar o valor em boolean
+- {isFetching && <div>Carregando...</div>} nesse trecho, usamos {logica && elemento}
+  para somente renderizar o elemento caso a logica seja verdadeira
 */
 
 const AppContent = ({ userinfo, repos, starred, handleSearch, getRepos, getStarred, isFetching }) => (
@@ -38,7 +41,11 @@ const AppContent = ({ userinfo, repos, starred, handleSearch, getRepos, getStarr
 AppContent.propTypes = {
   userinfo: PropTypes.object,
   repos: PropTypes.array.isRequired,
-  starred: PropTypes.array.isRequired
+  starred: PropTypes.array.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  handleSearch: PropTypes.func.isRequired,
+  getRepos: PropTypes.func.isRequired,
+  getStarred: PropTypes.func.isRequired
 }
 
 export default AppContent
